ef48534944bbb4fe3effe78adf7e786922f85da8
joshuamg41 <88290099+joshuamg41@users.noreply.github.com>
2023-03-16 19:56:53 -0400

    some change

--- src/components/Header/HeaderPage.tsx
+++ src/components/Header/HeaderPage.tsx
@@ -7,15 +7,15 @@ const HeaderPage = () => {
     <header className="headerContainer">
       <div className="headerTopContainer">
         <div className="headerContactIcons">
-        <i className="fa-regular fa-envelope"></i>
-        <i className="fa-brands fa-facebook"></i>
-        <i className="fa-brands fa-instagram"></i>
-        <i className="fa-brands fa-twitter"></i>
-        <i className="fa-brands fa-linkedin"></i>
-        <i className="fa-brands fa-tiktok"></i>
-        <i className="fa-brands fa-youtube"></i>
+          <i className="fa-regular fa-envelope"></i>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-tiktok"></i>
+          <i className="fa-brands fa-youtube"></i>
         </div>
-        <img src={"/logos/vacalogo.svg"} alt={"on"} style={{ height: 40 }} />
+        <img src={"/logos/vacalogo.svg"} alt={"on"} style={{ height: 65 }} />
         <div className="headerButtons">
           <Link className="secundaryButton" to="/">
             Acceder
@@ -26,27 +26,27 @@ const HeaderPage = () => {
         </div>
       </div>
       <nav className="headerNav">
-        <Link className="col text-center" to="/hotel">
-        <img src="/logos/hotelesicon.svg" style={{height:12}}/>  Hoteles
+        <Link className="col text-center" to="/">
+          Servicios
         </Link>
-        <Link className="col text-center" to="/ferrie">
-        <img src="/logos/cruseroicon.svg" style={{height:12}}/>  Cruceros
+        <Link className="col text-center" to="/excursiones">
+          Excursiones
+        </Link>
+        <Link className="col text-center" to="/trasports">
+          Trasporte
         </Link>
         <Link className="col text-center" to="/">
-        <img src="/logos/disneyicon.svg" style={{height:12}}/>   Disney
+          Ofertas
         </Link>
         <Link className="col text-center" to="/">
-        <img src="/logos/excursiones.svg" style={{height:12}}/>   Excursiones
+          Contáctanos
+        </Link>
+        {/* <Link className="col text-center" to="/">
+        <img src="/logos/ofertasicon.svg"  alt="icon"style={{height:12}}/>   Ofertas
         </Link>
         <Link className="col text-center" to="/">
-        <img src="/logos/vuelosicon.svg" style={{height:12}}/>    Vuelos
-        </Link>
-        <Link className="col text-center" to="/">
-        <img src="/logos/ofertasicon.svg" style={{height:12}}/>   Ofertas
-        </Link>
-        <Link className="col text-center" to="/">
-        <img src="/logos/ferrieicon.svg" style={{height:12}}/>    Ferrie
-        </Link>
+        <img src="/logos/ferrieicon.svg" alt="icon"style={{height:12}}/>    Ferrie
+        </Link> */}
       </nav>
     </header>
   );
